e494fbf3e1496e94235e4f717e4b9fad6ac335b1
Alex Caskey <floreat.bangoria@gmail.com>
2025-10-09 11:23:02 +0200

    Update LocalDateExt.kt

--- libs/domain/src/main/java/com/github/saikcaskey/pokertracker/domain/extensions/LocalDateExt.kt
+++ libs/domain/src/main/java/com/github/saikcaskey/pokertracker/domain/extensions/LocalDateExt.kt
@@ -1,10 +1,12 @@
 package com.github.saikcaskey.pokertracker.domain.extensions
 
+import kotlinx.datetime.DatePeriod
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.atStartOfDayIn
 import kotlinx.datetime.atTime
+import kotlinx.datetime.minus
 import kotlinx.datetime.toInstant
 import kotlin.time.Instant
 
@@ -14,4 +16,8 @@ fun LocalDate.atStartOfDayInstant(timeZone: TimeZone? = null): Instant {
 
 fun LocalDate.atTimeInstant(parsedTime: LocalTime, timeZone: TimeZone? = null): Instant {
     return atTime(parsedTime).toInstant(timeZone ?: TimeZone.currentSystemDefault())
+}
+
+fun LocalDate.minusDays(days: Int): LocalDate {
+    return this.minus(DatePeriod(days = days))
 }
