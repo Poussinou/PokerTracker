3d8ab4b6b79e19981a432fe5cd99ba318ee5a3ec
Alex Caskey <floreat.bangoria@gmail.com>
2025-10-09 11:25:20 +0200

    Update StatsFeatureContent.kt

--- feature/stats/src/main/java/com/github/saikcaskey/stats/presentation/StatsFeatureContent.kt
+++ feature/stats/src/main/java/com/github/saikcaskey/stats/presentation/StatsFeatureContent.kt
@@ -1,9 +1,14 @@
 package com.github.saikcaskey.stats.presentation
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.github.saikcaskey.ui_charts.presentation.BalanceByPeriodChart
 
 @Composable
 fun StatsFeatureContent(
@@ -17,8 +22,17 @@ fun StatsFeatureContent(
 fun StatsScreenContent(
     uiState: StatsFeatureComponent.UiState,
 ) {
-    Column {
-        Text("StatsScreenContent $uiState")
-        
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        Text("balanceDataSmall")
+        BalanceByPeriodChart(data = uiState.balanceDataSmall)
+        HorizontalDivider()
+        Text("balanceDataMedium")
+        BalanceByPeriodChart(data = uiState.balanceDataMedium)
+        HorizontalDivider()
+        Text("balanceDataLarge")
+        BalanceByPeriodChart(data = uiState.balanceDataLarge)
+        HorizontalDivider()
+        Text("uiState: $uiState")
+        HorizontalDivider()
     }
 }
